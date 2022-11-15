3e3bf02145600cde2705b67f8fa24caf7bc372d6
Stephen Souness <stephensouness@outlook.com>
2022-11-15 10:28:10 +0000

    task 3.GET/spi/categories  complete

---
+++ __tests__/app.test.js
@@ -0,0 +1,36 @@
+const request = require("supertest");
+const { get } = require("../app.js");
+const app = require("../app.js");
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data");
+const categories = require("../db/data/test-data/categories.js");
+const seed = require("../db/seeds/seed.js");
+
+afterAll(() => {
+  return db.end();
+});
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+describe.only("/api/categories", () => {
+  test("GET - 200: responds with an array of category objects, each with the following properties: slug, description", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        expect(categories.length).toBeGreaterThan(3);
+        expect(categories).toBeInstanceOf(Array);
+        categories.forEach((category) => {
+          expect(category).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
---
+++ controller/controller.js
@@ -0,0 +1,12 @@
+const { selectCategories } = require("../model/model.js");
+
+exports.getCategories = (req, res) => {
+  selectCategories()
+    .then((categories) => {
+      console.log(categories);
+      res.status(200).send({ categories });
+    })
+    .catch((err) => {
+      console.log("error msg", err);
+    });
+};
