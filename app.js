011bf7af1794f49bb7777d505a87b2779cce33f7
Stephen Souness <stephensouness@outlook.com>
2022-11-15 11:52:17 +0000

    task 3 - pushing amendments before changing branch

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { getCategories } = require("./controller/controller.js");
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/categories", getCategories);
 
 module.exports = app;
