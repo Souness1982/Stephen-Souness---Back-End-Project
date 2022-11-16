867555108490b86d10de21fae790bc0e3e56da1c
Stephen Souness <stephensouness@outlook.com>
2022-11-16 09:09:58 +0000

    Task 4 GET/api/Reviews complete

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,17 @@
-const { selectCategories } = require("../model/model.js");
+const { selectCategories, selectReviews } = require("../model/model.js");
 
 exports.getCategories = (req, res) => {
   selectCategories()
     .then((categories) => {
       res.status(200).send({ categories });
+    })
+    .catch((err) => {});
+};
+
+exports.getReviews = (req, res) => {
+  selectReviews()
+    .then((reviews) => {
+      res.status(200).send({ reviews });
     })
     .catch((err) => {});
 };
--- model/model.js
+++ model/model.js
@@ -5,3 +5,19 @@ exports.selectCategories = () => {
     return res.rows;
   });
 };
+
+exports.selectReviews = () => {
+  return db
+    .query(
+      `SELECT name, reviews.review_id, reviews.title, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comments.comment_id) FROM reviews
+      JOIN users
+      ON reviews.owner = users.username
+      LEFT JOIN comments
+      ON comments.review_id = reviews.review_id 
+      GROUP BY users.name, reviews.review_id
+      ORDER BY created_at DESC;`
+    )
+    .then((res) => {
+      return res.rows;
+    });
+};
